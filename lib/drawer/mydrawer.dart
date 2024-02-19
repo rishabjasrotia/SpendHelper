5926607a14958fad3a260003597311f96143f65c
Rishab <33950743+rishabjasrotia@users.noreply.github.com>
2024-02-19 10:00:41 +0530

    Create mydrawer.dart

---
+++ lib/drawer/mydrawer.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  MyDrawer(this.currentPage);
+
+  final String currentPage;
+  
+  @override
+  Widget build(BuildContext context){
+    return Drawer(
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('SpendHelper'),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.home,
+            ),
+            title: const Text('Page 1'),
+            onTap: () {
+              selectedItem(context, 1);
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.train,
+            ),
+            title: const Text('Page 2'),
+            onTap: () {
+              selectedItem(context, 2);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void selectedItem(BuildContext context, int index) {
+  // Navigator.of(context).pop();
+  switch (index) {
+    case 0:
+      Navigator.pushReplacementNamed(context, "/personalExpense");
+      break;
+    case 1:
+      Navigator.pushReplacementNamed(context, "/personalExpense");
+      break;
+    case 2:
+      Navigator.pushReplacementNamed(context, "/personalExpense");
+      break;
+  }
+}
