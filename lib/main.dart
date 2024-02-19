398c7dc8869207b4c34164a2db4fd1c285712441
Rishab <33950743+rishabjasrotia@users.noreply.github.com>
2024-02-19 10:36:57 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:spendhelper/src/gsheets.dart';
 import 'package:spendhelper/widgets/familyexpense.dart';
+import 'package:spendhelper/widgets/personalexpense.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:async';
 
@@ -18,7 +19,7 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(useMaterial3: true),
       home: const FamilyExpense(),
       routes: {
-        '/personalExpense': (context) => const FamilyExpense(),
+        '/personalExpense': (context) => const PersonalExpense(),
       },
     );
   }
