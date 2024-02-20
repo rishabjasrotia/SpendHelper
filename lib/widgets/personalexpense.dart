b0303441eca5538325f32eb1cc778d8bd5e100ec
Rishab <33950743+rishabjasrotia@users.noreply.github.com>
2024-02-20 10:34:26 +0530

    Update personalexpense.dart

--- lib/widgets/personalexpense.dart
+++ lib/widgets/personalexpense.dart
@@ -20,7 +20,7 @@ Future<List<Data>> fetchData() async {
   var epoch = new DateTime(1899, 12, 30);
 
   List jsonResponse = [];
-  for (var i = 0; i < personalDates!.length; i++) {
+  for (var i = personalDates!.length - 1; i > 0; i--) {
     // TO DO
     var internal = {
       'date':
