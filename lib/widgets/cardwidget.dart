3c70b455ced71500cf3f589ba975f9b4a419a36d
Rishab <33950743+rishabjasrotia@users.noreply.github.com>
2024-02-20 10:33:56 +0530

    Update cardwidget.dart

--- lib/widgets/cardwidget.dart
+++ lib/widgets/cardwidget.dart
@@ -6,13 +6,30 @@ import 'package:spendhelper/handler/gsheethandler.dart';
 double defaultRadius = 8.0;
 const double _cardWidth = 115;
 
-Future<List> fetchFutureTotal() async {
+Future<List> fetchFamilyTotal() async {
   final ss = await gSheetLoader();
   // get worksheet by its title
   var sheet = ss.worksheetByTitle('Overall');
   var familyTotalExpense = await sheet?.values.value(column: 4, row: 2);
   return Future.value([familyTotalExpense, 'Family Expense']);
 }
+
+Future<List> fetchPersonalTotal() async {
+  final ss = await gSheetLoader();
+  // get worksheet by its title
+  var sheet = ss.worksheetByTitle('Overall');
+  var personalTotalExpense = await sheet?.values.value(column: 11, row: 2);
+  return Future.value([personalTotalExpense, 'Personal Expense']);
+}
+
+Future<List> fetchCreditTotal() async {
+  final ss = await gSheetLoader();
+  // get worksheet by its title
+  var sheet = ss.worksheetByTitle('Overall');
+  var creditTotalExpense = await sheet?.values.value(column: 17, row: 2);
+  return Future.value([creditTotalExpense, 'Credit Card Expense']);
+}
+
 
 class CardBasicRoute extends StatefulWidget {
   const CardBasicRoute({super.key});
@@ -35,103 +52,9 @@ class CardBasicRouteState extends State<CardBasicRoute> {
         scrollDirection: Axis.vertical,
         child: Column(
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    //color: Colors.teal[800],
-                    color: MyColorsSample.primary,
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          padding: const EdgeInsets.all(15),
-                          child: Text(
-                            "Family Exepnse\n",
-                            style: TextStyle(fontSize: 24, color: Colors.white),
-                          ),
-                        ),
-                        const Divider(
-                          color: Colors.white,
-                          thickness: 0.5,
-                          height: 0,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Container(
-                              width: 15,
-                            ),
-                            const Text(
-                              "March 19, 2023",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            const Spacer(),
-                            IconButton(
-                              icon:
-                                  const Icon(Icons.event, color: Colors.white),
-                              onPressed: () {},
-                            ),
-                            Container(
-                              width: 4,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(width: 2),
-                Expanded(
-                  flex: 1,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    color: MyColorsSample.primary,
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Container(
-                              width: 15,
-                            ),
-                            const Text(
-                              "Call",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            const Spacer(),
-                            IconButton(
-                              icon: const Icon(Icons.call, color: Colors.white),
-                              onPressed: () {},
-                            ),
-                            Container(
-                              width: 4,
-                            ),
-                          ],
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(15),
-                          child: const Text(
-                            "John Smith \nTek",
-                            style: TextStyle(fontSize: 24, color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            familyExpenseCard(),
             Container(height: 10),
-            //..................................................................gradient card view
-            gradientCardSample(),
+            personalExpenseCard(),
           ],
         ),
       ),
@@ -139,14 +62,14 @@ class CardBasicRouteState extends State<CardBasicRoute> {
     );
   }
 
-  Widget gradientCardSample() {
+  Widget familyExpenseCard() {
     return FutureBuilder(
-      future: fetchFutureTotal(),
+      future: fetchFamilyTotal(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
 
           return Container(
-            height: 200,
+            height: 150,
             width: double.infinity,
             padding: const EdgeInsets.all(32),
             decoration: BoxDecoration(
@@ -156,6 +79,136 @@ class CardBasicRouteState extends State<CardBasicRoute> {
                   colors: [
                     Color(0xFF846AFF),
                     Color(0xFF755EE8),
+                    Colors.purpleAccent,
+                    Colors.amber,
+                  ],
+                ),
+                borderRadius: radius(16)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(snapshot.data![1],
+                            style: MyTextSample.headline(context)!.copyWith(
+                                color: Colors.white,
+                                fontFamily:
+                                    "monospace")), //, style: boldTextStyle(color: Colors.white, size: 20)
+                        const Spacer(),
+                        Stack(
+                          children: List.generate(
+                            2,
+                            (index) => Container(
+                              margin: EdgeInsets.only(left: (15 * index).toDouble()),
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                  borderRadius: radius(100), color: Colors.white54),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Text(snapshot.data![0],
+                  style: TextStyle(fontSize: 24, color: Colors.white))
+              ],
+            ),
+          );
+        }
+         // By default show a loading spinner.
+        return const CircularProgressIndicator();
+      }
+    );
+  }
+
+  Widget personalExpenseCard() {
+    return FutureBuilder(
+      future: fetchPersonalTotal(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+
+          return Container(
+            height: 150,
+            width: double.infinity,
+            padding: const EdgeInsets.all(32),
+            decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xff283593),
+                    Color(0xff1976d2),
+                    Colors.purpleAccent,
+                    Colors.amber,
+                  ],
+                ),
+                borderRadius: radius(16)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(snapshot.data![1],
+                            style: MyTextSample.headline(context)!.copyWith(
+                                color: Colors.white,
+                                fontFamily:
+                                    "monospace")), //, style: boldTextStyle(color: Colors.white, size: 20)
+                        const Spacer(),
+                        Stack(
+                          children: List.generate(
+                            2,
+                            (index) => Container(
+                              margin: EdgeInsets.only(left: (15 * index).toDouble()),
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                  borderRadius: radius(100), color: Colors.white54),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Text(snapshot.data![0],
+                  style: TextStyle(fontSize: 24, color: Colors.white))
+              ],
+            ),
+          );
+        }
+         // By default show a loading spinner.
+        return const CircularProgressIndicator();
+      }
+    );
+  }
+
+   Widget creditExpenseCard() {
+    return FutureBuilder(
+      future: fetchCreditTotal(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+
+          return Container(
+            height: 150,
+            width: double.infinity,
+            padding: const EdgeInsets.all(32),
+            decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xff283593),
+                    Color(0xff1976d2),
                     Colors.purpleAccent,
                     Colors.amber,
                   ],
